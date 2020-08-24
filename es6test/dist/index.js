b3bcc66e2c1003462aa71669a4fbac8c16402051
Yuehua <wuyuehua5@gmail.com>
2020-08-24 15:36:40 +0800

    new code

---
+++ es6test/dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+ * @Author: George Wu
+ * @Date: 2020-08-23 22:48:27
+ * @LastEditors: George Wu
+ * @LastEditTime: 2020-08-24 15:29:33
+ * @FilePath: \es6test\src\index.js
+ */
+var a = "George Wu";
+
+{
+  var _a = "wuyuehua";
+}
+
+console.log(a);
---
+++ es6test/src/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: George Wu
+ * @Date: 2020-08-23 22:48:27
+ * @LastEditors: George Wu
+ * @LastEditTime: 2020-08-24 15:29:33
+ * @FilePath: \es6test\src\index.js
+ */
+let a = "George Wu";
+
+{
+    let a = "wuyuehua";
+}
+
+console.log(a);
