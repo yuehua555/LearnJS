0adef1d50a7b32a353fe3cb94df461854c72ee84
Yuehua <wuyuehua5@gmail.com>
2020-09-03 15:49:11 +0800

    jQuery: dom

---
+++ jQuery/dom/jQuery-3.5.1.js
@@ -0,0 +1,459 @@
+/*
+ * @Author: George Wu
+ * @Date: 2020-08-26 17:15:34
+ * @LastEditors: George Wu
+ * @LastEditTime: 2020-09-03 15:36:32
+ * @FilePath: \dom\jQuery-3.5.1.js
+ */
+(function (global, factory) {
+    "use strict";
+
+	if ( typeof module === "object" && typeof module.exports === "object" ) {
+
+		// For CommonJS and CommonJS-like environments where a proper `window`
+		// is present, execute the factory and get jQuery.
+		// For environments that do not have a `window` with a `document`
+		// (such as Node.js), expose a factory as module.exports.
+		// This accentuates the need for the creation of a real `window`.
+		// e.g. var jQuery = require("jquery")(window);
+		// See ticket #14549 for more info.
+		module.exports = global.document ?
+			factory( global, true ) :
+			function( w ) {
+				if ( !w.document ) {
+					throw new Error( "jQuery requires a window with a document" );
+				}
+				return factory( w );
+			};
+	} else {
+		factory( global );
+	}
+})(typeof window !== "undefined" ? window : this, function( window, noGlobal){
+
+	var isFunction = function isFunction( obj ) {
+		return typeof obj === "function" && typeof obj.nodeType !== "number";
+	};
+
+	var jQuery = function (selector, context) {
+		return new jQuery.prototype.init(selector, context);
+	};
+	
+	jQuery.fn = jQuery.prototype = {
+		length: 0,
+		init: function (selector, context) {
+			if (!selector) {
+				return this;
+			}
+
+			if (typeof selector === 'string') {
+				let elems = document.querySelectorAll(selector);
+				for (let i = 0; i < elems.length; i++) {
+					this[i] = elems[i];
+				}
+				this.length = elems.length;
+				this.selector = selector;
+				this.context = document;
+				return this;
+			}
+
+			if (selector.nodeType) {
+				this.context = this[0] = selector;
+				this.length = 1;
+				return this;
+			}
+		},
+		css: function () {
+			
+		},
+		//... 扩展
+	}
+
+	// 生态， extend 1. 对内，2. 对外
+	jQuery.prototype.init.prototype = jQuery.fn;
+
+	// $.extend() $.fn.extend() $.extend({}, def, options); this
+	// 1。参数不固定 有可能是一个， 有可能是多个
+	jQuery.extend = jQuery.fn.extend = function () {
+
+		var option, name;
+		var target = arguments[0] || {}; // 确保第一个参数有值，
+		var i = 1;
+		var length = arguments.length;
+		var deep;
+		var copy, src;
+		var clone;
+
+		// $.extend({}, def, options); 确定按照原有的逻辑给target 扩展
+		if (typeof target === 'boolean') {
+			deep = target;
+			target = arguments[1] || {};
+		}
+
+		//  第一个或第二个（如果第一个为布尔值）参数 并且为对象
+		if (typeof target !=='object' && !isFunction(target)) {
+			target = {};
+		}
+
+		if (i === length) {
+			target = this; //$.extend() this => jQuery 本身 $.fn.extend() this => jQuery 实例对象
+			i--;
+		}
+		
+		for (; i < length; i++) {
+			if(((option = arguments[i]) != null)) { // undefined
+				for (name in option) {
+					//console.log(name);
+					copy = option[name];  // 遍历的对象属性值
+					src = target[name];   // 扩展的对象属性值
+					if (deep && ( jQuery.isPlaninObject(copy) || (copyIsArray = jQuery.isArray(copy)))) {
+						if (copyIsArray) {
+							// copy 是一个数组
+							clone = src && jQuery.isArray(src) ? src : [];
+							copyIsArray = false;
+						} else {
+							// copy 是一个对象
+							clone = src && jQuery.isPlaninObject(src) ? src : {};	
+						} 
+						// 浅拷贝
+						target[name] = jQuery.extend(deep, clone, copy);
+					} else if (copy !== undefined) {
+						//console.log(target);
+						if (toString.call(target) === '[object Array]') {
+							//target[name] = copy;
+							// console.log('src = ' + src);
+							// console.log(target);
+							// console.log(name);
+							// console.log(copy);
+							//target[name] = copy;
+							if (src !== copy) {
+								target.push(copy);
+							}
+							
+						} else {
+							target[name] = copy;
+						}
+										
+					}
+				}
+
+			}
+		}
+
+		return target;
+		
+	}
+	
+	jQuery.extend({
+		isPlaninObject: function (obj) {
+			return toString.call(obj) === '[object Object]';
+		},
+		isArray: function (obj) {
+			return toString.call(obj) === '[object Array]';
+		}
+	});
+
+
+	var optionsCache = {};
+	jQuery.Callbacks = function(options) {// once stopOnFalse memory
+
+		options = typeof options === 'string' ? (optionsCache[options] || createOptions(options)) : {};
+		var list = [];
+		var index, length, testings;
+		var start, original;
+		var memory;
+
+		var fire = function(data) {
+
+		    memory = options.memory && data;
+
+			index = start || 0;
+			start = 0;
+			testings = true;
+			length = list.length;
+			for (; index < length; index++) {{
+				//console.log(data[0]);
+				if (list[index].apply(data[0], data[1]) === false && options.stopOnFalse) { // ==隐式转化
+					break;
+				}
+			}}
+
+		}
+
+		var self = {
+			add: function(){
+				// arguments 类数组 -> 转化成数组对象 forEach
+				var args = [].slice.call(arguments);
+				original = list.length;
+				args.forEach(function(fun) {
+					//console.log(fun);
+					if (isFunction(fun) && list.indexOf(fun) === -1) {
+						list.push(fun);
+					}
+				});
+				
+				if (memory) {
+					start = original;
+					fire(memory);
+				}
+
+				return this;
+			},
+			// 给所有的回调函数传参 
+			fire: function(){
+				self.fireWith(this, arguments);
+				return this;
+			},
+			// 给所有的回调函数绑定上下文 this
+			fireWith: function(context, args){
+				var parameters = [context, args];
+				if (!(options.once) || !testings) {
+					fire(parameters);
+				}	
+			}
+		};
+
+		return self;
+	}
+
+	jQuery.extend({
+		Deferred: function (func) {
+			// [resolve | reject | notify]  fire
+			// [done | fail | progress]  add
+			let tuples = [
+				['resolve', 'done', jQuery.Callbacks('memory once'), 'resolved'],
+				['reject', 'fail', jQuery.Callbacks('memory once'), 'rejected'],
+				['notify', 'progress', jQuery.Callbacks('memory once')]
+			];
+			let state = 'pending';
+			let promise = {
+				state: function() {
+					return state;
+				},
+				always: function() {
+
+				},
+				then: function(/* fnDone, fnFail, fnProgress*/) {
+					let fns = arguments;
+					 return jQuery.Deferred(function(newDef){ // 构建一个全新的deferred对象
+						tuples.forEach(function(tuple, i){
+							let fn = isFunction(fns[i]) && fns[i];
+							// deferred === first newDef === second
+							deferred[tuple[1]](function(){
+								let returned = fn && fn.apply(this, arguments);
+								if (returned && isFunction(returned.promise)) {
+									returned.promise()
+									.done(newDef.resolve)
+									.fail(newDef.reject)
+									.progress(newDef.notify);
+								}
+							});
+						});
+
+					}).promise();
+
+				},
+				
+				promise: function(obj) {
+					return obj != null ? jQuery.extend(obj, promise) : promise;
+				}
+			};
+			let deferred = {};
+
+			tuples.forEach(function(tuple) {
+				let list = tuple[2];
+				//console.log(list);
+				let stateString = tuples[3];
+				if (stateString) {
+					list.add(function() {
+						// [resolved | rejected ] = state
+						state = stateString;
+					});
+				}
+				
+				// promise[done | fail | progress]
+				// 只会往容器里添加回调函数，不存在状态绑定
+				promise[tuple[1]] = list.add;
+			
+				// //deferred [resolve | reject | notify]
+				deferred[tuple[0]] = function() { //权限控制
+					deferred[tuple[0] + "With"](this === deferred ? promise : this, arguments);  //["..."]
+				}
+				//deferred[resolveWith | rejectWith | notifyWith ]
+				deferred[tuple[0] + "With"] = list.fireWith;
+			});
+
+			if (func) {
+				func.call(deferred, deferred);
+			}
+			promise.promise(deferred);
+			return deferred;
+		},
+		when: function(subordinate) {
+			let resolveValue = [].slice.call(arguments);
+			let length = resolveValue.length;
+			let i = 0;
+			let remaining = length !==1 || (subordinate && isFunction(subordinate.promise)) ? length : 0;
+			
+			deferred = length === 1 ? subordinate : jQuery.Deferred();
+			if (length > 1) {
+				for (;i < length; i++) {
+					if (resolveValue[i] && isFunction(resolveValue[i].promise)) {
+						resolveValue[i].promise()
+						.done(function() {
+							if (!(--remaining)) {
+								deferred.resolveWith(this, arguments);
+							}
+						})
+						.fail(deferred.reject);
+					}
+				}
+			}
+					
+			
+			return deferred.promise();
+		},
+		style: function(elem, key, value) {
+			elem.style[key] = value;
+		},
+		css: function(elem, key) {
+			let computed = window.getComputedStyle(elem);
+			return computed.getPropertyValue(key);
+		}
+	});
+
+	let noInnerHTML = /<script | <style | <link /i;
+	jQuery.fn.extend({
+		html: function(value){
+			return access(this, function(value) {
+				let elem = this[0] || {};
+				let i = 0;
+				let len = this.length;
+
+				// get
+				if (value === undefined) {
+					return elem.innerHTML;
+				}
+
+				// set
+				if (typeof value === 'string' && !noInnerHTML.test(value)) {
+					for (; i < len; i++) {
+						elem = this[i];
+						if (elem.nodeType === 1) {
+							elem.innerHTML = value;
+						}
+					}
+				}
+			}, null, value, arguments.length);
+		},
+		css: function(key, value){
+			return access(this, function(elem, key, value){
+				let i = 0;
+				let map = [];
+				//let computed;
+				
+				//console.log(elem)
+				if (Array.isArray(key)){
+					let computed = window.getComputedStyle(elem);
+					//console.log(computed);
+					for (; i < key.length; i++) {
+						map[key[i]] = computed.getPropertyValue(key[i]);
+					}
+					return map;
+				}
+				return value != undefined ? jQuery.style(elem, key, value) : jQuery.css(elem, key);
+			}, key, value, arguments.length>1);
+			
+		}
+	});
+
+	let class2type = {};
+
+	'Boolean Number String Function Array Date RegExp Object Error Symbol'.split(' ').forEach(function(name){
+		class2type['[object ' + name + ']'] = name.toLowerCase();	
+	});
+	function toType(obj) {
+		//console.log(class2type);
+		return typeof obj === 'object' || typeof obj === 'function' ?
+			class2type[toString.call(obj)] || 'object' :
+			typeof obj;
+	}
+
+	// 用来获取和设置集合值的多功能的方法
+	let access = function(elems, fn, key, value, chainable, emptyget, raw) {
+		let bulk = key == null;
+		let len = elems.length;
+		let i = 0;
+
+		// sets many value
+		if (toType(key) === 'object') {
+			chainable = true;
+			for (i in key) {
+				access(elems, fn, i, key[i], true, emptyget, raw);
+			}
+
+
+          // sets one value           
+		} else if (value != undefined) {
+			chainable = true;
+
+			if (!isFunction(value)) {
+				raw = true;
+			}
+			if (bulk) {
+				if (raw) {
+					fn.call(elems, value);
+					fn = null;
+				} else {
+					bulk = fn;
+					// 包装的函数
+					fn = function(elems, key, value) {
+						return bulk.call(jQuery(elems), value);
+					};
+				}
+			}
+
+			
+			if (fn) {
+				for (; i < len; i++) {
+					fn(elems[i], key, raw ? value : value.call(elems[i], i));
+				}
+			}	
+		}
+
+		if (chainable) {
+			return elems;
+		}
+		
+		// get
+		if (bulk) {
+		   return fn.call(elems);
+		}
+
+		return len ? fn(elems[0], key) : emptyget;
+
+	};
+	// commonjs 规定模块长什么样子
+	// requirejs seajs 模块加载器的工具
+	if ( typeof define === "function" && define.amd ) {
+		define( "jquery", [], function() {
+			return jQuery;
+		} );
+	}
+
+	if ( typeof noGlobal === "undefined" ) {
+		window.jQuery = window.$ = jQuery;
+	}
+	
+
+	function createOptions(options) {
+		var object = optionsCache[options] = {};
+		//["memory","once"]
+		options.split(/\s+/).forEach(function(value) {
+			object[value] = true;
+		});
+	
+		return object;
+	}
+
+	return jQuery;
+})
